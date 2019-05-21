de465bf805494af27496a6a10d087e332560be48
yazid64 <ainurkhafid64@gmail.com>
2019-05-21 16:37:58 +0700

    angger

--- app/src/main/java/rpl2016_17/example/com/salesmanmake2/UserProfile.java
+++ app/src/main/java/rpl2016_17/example/com/salesmanmake2/UserProfile.java
@@ -8,6 +8,6 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        setContentView(R.layout.fragment_profile);
     }
 }
