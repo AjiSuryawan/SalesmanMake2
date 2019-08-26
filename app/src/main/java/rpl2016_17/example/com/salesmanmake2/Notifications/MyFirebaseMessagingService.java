5b5b7db6fcfaa8aae202faa75a5ffa51a59a6b27
hasanwijaya <hasanwijaya.xprl2018smkrus@gmail.com>
2019-08-26 13:44:46 +0700

    sycned

---
+++ app/src/main/java/rpl2016_17/example/com/salesmanmake2/Notifications/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package rpl2016_17.example.com.salesmanmake2.Notifications;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
